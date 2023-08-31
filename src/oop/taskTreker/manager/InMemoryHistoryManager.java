5dcf16b34556f8b7e3ba4ec26244f9dd169292a2
Soyl <Soylgr@yandex.ru>
2023-08-31 18:48:45 +0300

    new:добавил все что просили вроде :3

--- src/oop/taskTreker/manager/InMemoryHistoryManager.java
+++ src/oop/taskTreker/manager/InMemoryHistoryManager.java
@@ -3,25 +3,91 @@ package oop.taskTreker.manager;
 import oop.taskTreker.task.Task;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final ArrayList<Task> historyOfViews = new ArrayList<>();
+    //private final ArrayList<Task> historyOfViews = new ArrayList<>();
     private final Integer LIMIT_OF_HISTORY_SIZE = 10;
+    private final Map<Long,Node> nodeMap = new HashMap<>();
+    private Node first;
+    private Node last;
+
+
+    @Override
+    public ArrayList<Task> getHistory() {
+        return getTasks();
+    }
 
     @Override
     public void add(Task task) {
         if (task == null) {
             return;
         }
-        historyOfViews.add(task);
-        if (historyOfViews.size() >= LIMIT_OF_HISTORY_SIZE) {
-            historyOfViews.remove(0);
-        }
+
+        long id = task.getId();
+        remove(id);
+        linkLast(task);
+        nodeMap.put(id,last);
     }
 
     @Override
-    public ArrayList<Task> getHistory() {
-        return historyOfViews;
+    public void remove(long id) {
+        Node node = nodeMap.get(id);
+        if(node == null) {
+            return;
+        }
+        removeNode(node);
+    }
+
+    public void removeNode(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+            if (node.next == null) {
+                last = node.prev;
+            } else {
+                node.next.prev = node.prev;
+            }
+        } else {
+            first = node.next;
+            if (first == null) {
+                last = null;
+            } else {
+                first.prev = null;
+            }
+        }
+    }
+
+    private void linkLast(Task task) {
+        Node node = new Node(task,last,null);
+        if (first == null) {
+           first = node;
+        }else {
+            last.next = node;
+        }
+        last = node;
+    }
+
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        Node node = first;
+        while (node != null) {
+            tasks.add(node.task);
+            node = node.next;
+        }
+        return tasks;
+    }
+
+
+    public static class Node {
+        Task task;
+        Node prev; // предыдущий
+        Node next; // следующий
+
+        public Node(Task task, Node first, Node last) {
+            this.task = task;
+            this.prev = first;
+            this.next = last;
+        }
     }
 }
